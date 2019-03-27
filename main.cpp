b098047528c928a5cc859689e89803dd99f8e9de
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-03-27 18:26:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-int bcount =0;
-
-
 int main(){
-	char character = 'b';
+	int bcount=0;
 	string str;
 	cout << "Type a line: ";
 	getline(cin, str);
 	
 	for (int i=0; i < str.size(); i++){
-		if (str[i] == character){
+		if (str[i] == 'b'){
 			bcount++;
+			if(bcount%2 == 1){
+				cout << "SIT";
+			}else{
+				cout << "STAND";
+			}
+		}else {
+			cout << str[i];
 		}
 	}
-	
-	
-	
-	
 	return 0;
 }
